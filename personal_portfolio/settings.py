d4a2965980af6fff513d93bd98a43440e3bf73b9
no-kris <kris.tresk@gmail.com>
2025-03-20 16:41:08 -0400

    Update: Added blue background gradient to hero section in the home page.

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w6*%#p!(ktbu1@6-0=6du7(wd4#b6%1n)#4)%l!xs3&+j=ct3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STAIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'uploads/'
